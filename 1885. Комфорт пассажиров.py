8bf3de59d029c337a31049d3cb0aec9351db7d1a
Dami178 <151924972+Dami178@users.noreply.github.com>
2025-10-12 18:46:36 +0500

    Create 	1885. Комфорт пассажиров.py

---
+++ 1885. Комфорт пассажиров.py
@@ -0,0 +1,14 @@
+h,t,v,x=[int(a) for a in input().split()]
+vmin=h/t
+
+if vmin >=x:
+    tmin=(t*x -h)/(v-x)
+else:
+    tmin=0
+
+if vmin>x:
+    tmax=t
+else:
+    tmax=h/x
+
+print(abs(tmin),tmax)
